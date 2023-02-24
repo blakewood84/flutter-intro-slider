cfd8a3ac5bf0ed9acfb73b336d8bb015ce5d87bc
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-02-24 10:44:28 -0800

    added PageView and DotsIndicator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:carousel_slider/main.dart';
+import 'package:intro_slider/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
